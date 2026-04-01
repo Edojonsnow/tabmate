e066c49f56d2555fdb78a8592a9992c9185b31de
Alexander Oronsaye <osahenrumeh13@gmail.com>
2026-04-01 12:43:46 +0100

    Add rate limiting to ScanMenu

--- internals/controllers/menu/handler.go
+++ internals/controllers/menu/handler.go
@@ -1,17 +1,39 @@
 package menucontroller
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"tabmate/internals/menu"
+	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	scanMu   sync.Mutex
+	lastScanAt = make(map[string]time.Time)
 )
 
 // ScanMenu accepts a multipart image upload and returns parsed menu items.
 // POST /api/tables/:code/scan-menu
 func ScanMenu() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		userID := c.MustGet("user_id")
+		uid := fmt.Sprintf("%v", userID)
+
+		scanMu.Lock()
+		lastTime, ok := lastScanAt[uid]
+		if ok && time.Since(lastTime) < 30*time.Second {
+			scanMu.Unlock()
+			c.JSON(http.StatusTooManyRequests, gin.H{"error": "Please wait 30 seconds before scanning again"})
+			return
+		}
+		lastScanAt[uid] = time.Now()
+		scanMu.Unlock()
+
 		file, header, err := c.Request.FormFile("menu_image")
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": "menu_image file is required"})
